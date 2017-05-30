2f914758a0c6ed2a075abfb10dea16fbca77e8af
a76597 <gregory.anne@bnpparibas.fr>
2017-05-30 11:45:00 +0200

    i want to build in a different folder, will use gulpfile.js

---
+++ v5/gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var del = require('del');
+var tsc = require('gulp-tsc');
+var shell = require('gulp-shell');
+var runseq = require('run-sequence');
+var tslint = require('gulp-tslint');
+
+var paths = {
+    tscripts: {
+        src: ['src/**/*.ts'],
+        dest: 'dist'
+    }
+};
+
+// clean the contents of the distribution directory
+gulp.task('clean', function () {
+  return del('dist/**/*');
+});
+
+gulp.task('default', ['lint', 'buildrun']);
+
+// ** Running ** //
+gulp.task('run', shell.task([
+    'node dist/index.js'
+]));
+
+gulp.task('buildrun', function (cb) {
+    runseq('build', 'run', cb);
+});
+
+// ** Watching ** //
+
+gulp.task('watch', function () {
+    gulp.watch(paths.tscripts.src, ['compile:typescript']);
+});
+
+gulp.task('watchrun', function () {
+    gulp.watch(paths.tscripts.src, runseq('compile:typescript', 'run'));
+});
+
+// ** Compilation ** //
+
+gulp.task('build', ['compile:typescript']);
+gulp.task('compile:typescript', function () {
+    return gulp
+            .src(paths.tscripts.src)
+            .pipe(tsc({
+                module: "commonjs",
+                emitError: false
+            }))
+            .pipe(gulp.dest(paths.tscripts.dest));
+});
+
+// ** Linting ** //
+
+gulp.task('lint', ['lint:default']);
+gulp.task('lint:default', function () {
+    return gulp.src(paths.tscripts.src)
+            .pipe(tslint())
+            .pipe(tslint.report('prose', {
+                emitError: false
+            }));
+});
