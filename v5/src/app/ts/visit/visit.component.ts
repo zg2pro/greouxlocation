9102f7ce87eca76d622c3aae16fc30af5155e601
a76597 <gregory.anne@bnpparibas.fr>
2017-06-19 12:31:31 +0200

    some more pictures

--- v5/src/app/ts/visit/visit.component.ts
+++ v5/src/app/ts/visit/visit.component.ts
@@ -13,7 +13,8 @@ export class VisitComponent extends AbstractContentComponent {
     constructor() {
         super();
         let listNames = ["IM000521", "IM000523", "im000043", "im000044", "im000057",
-            "im000058", "im000060", "im000066", "im000069", "im000073", "im000076"];
+            "im000058", "im000060", "im000066", "im000069", "im000073", "im000076",
+            "jm1", "jm2", "jm3", "jm4", "jm5"];
         this.myImages = [];
         for (let entry of listNames) {
             let ii = new ImageInterface(entry);
