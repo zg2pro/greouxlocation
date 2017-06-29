47d0f7abdd7cd22326da746398b913df0ab4e5f9
a76597 <gregory.anne@bnpparibas.fr>
2017-06-29 14:25:40 +0200

    i18n

--- v5/src/app/ts/fares/fares.module.ts
+++ v5/src/app/ts/fares/fares.module.ts
@@ -4,13 +4,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Route, RouterModule} from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FaresDatepicker} from './bootstrap/datepicker.component';
-import {NgbDatepickerModule, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {MODULE_COMPONENTS, MODULE_ROUTES} from './fares.routes';
 
 @NgModule({
     imports: [NgbModule, CommonModule, FormsModule, ReactiveFormsModule, NgbDatepickerModule, RouterModule.forChild(MODULE_ROUTES)],
     declarations: [MODULE_COMPONENTS, FaresDatepicker]
-    //,exports: [CommonModule, ReactiveFormsModule, FormsModule]
 })
 export class FaresModule {}
