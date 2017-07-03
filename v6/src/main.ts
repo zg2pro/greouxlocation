6f9302316ac566a41ceba979577902dd3e0feada
a76597 <gregory.anne@bnpparibas.fr>
2017-07-03 15:32:49 +0200

    systemjs

--- v6/src/main.ts
+++ v6/src/main.ts
@@ -2,10 +2,15 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { getTranslationProviders } from './app/i18n.providers';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+getTranslationProviders().then(providers => {
+  const options = { providers };
+  platformBrowserDynamic().bootstrapModule(AppModule, options);
+});
+
