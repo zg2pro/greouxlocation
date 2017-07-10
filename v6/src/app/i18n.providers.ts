7d24eed18e680242535be656c6f568ffe9e055d6
Gregory Anne <englishblazere@free.fr>
2017-07-10 23:38:46 +0200

    redo i18n

--- v6/src/app/i18n.providers.ts
+++ v6/src/app/i18n.providers.ts
@@ -1,5 +1,6 @@
 import {TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID} from '@angular/core';
 import {Observable} from "rxjs/Rx";
+//import * as fs from 'fs';
 
 export function getTranslationProviders(): Promise<Object[]> {
     // Get the locale id from the global
@@ -12,7 +13,7 @@ export function getTranslationProviders(): Promise<Object[]> {
     }
     // Ex: 'locale/messages.fr.xlf`
     const translationFile = `./assets/locale/messages.${locale}.xlf`;
-    var provider = getTranslationsWithoutSystemJs(translationFile)
+    let provider = getTranslationsWithoutSystemJs(translationFile)
         .then((translations: string) => [
             {provide: TRANSLATIONS, useValue: translations},
             {provide: TRANSLATIONS_FORMAT, useValue: 'xlf'},
@@ -20,27 +21,33 @@ export function getTranslationProviders(): Promise<Object[]> {
         ])
         .catch(() => noProviders); // ignore if file not found
 
-   // debugger;
+    // debugger;
     return provider;
 }
 
 function getTranslationsWithoutSystemJs(file: string) {
+//    return new Promise(function(resolve, reject) {
+//        fs.readFile(file, function(err, data){
+//            if (err) 
+//                reject(err); 
+//            else 
+//                resolve(data);
+//        });
+//    });
     // changes Start here 
-    var text = "";
-    var fileRequest = new XMLHttpRequest();
-    fileRequest.open("GET", file, false);
+    let text = "";
+    let fileRequest = new XMLHttpRequest();
+    fileRequest.open("GET", file, true);
     fileRequest.onerror = function (err) {
         console.log(err);
     }
     fileRequest.onreadystatechange = function () {
-        if (fileRequest.readyState === 4) {
-            if (fileRequest.status === 200 || fileRequest.status == 0) {
-                text = fileRequest.responseText;
-            }
+        if (fileRequest.readyState === 4 &&
+            (fileRequest.status === 200 || fileRequest.status == 0)) {
+            text = fileRequest.responseText;
         }
     }
-    fileRequest.send()
-    var observable = Observable.of(text);
-    var prom = observable.toPromise();
-    return prom;
+    fileRequest.send();
+    let observable = Observable.of(text);
+    return observable.toPromise();
 }
