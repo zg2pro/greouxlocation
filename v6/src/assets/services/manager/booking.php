a9b763c2f9d3e71babce84c16f81f47c8d9a62ae
Gregory Anne <englishblazere@free.fr>
2017-07-11 21:04:18 +0200

    pb carousel bootstrap sur chrome

--- v6/src/assets/services/manager/booking.php
+++ v6/src/assets/services/manager/booking.php
@@ -2,29 +2,27 @@
 
 require_once '../../security/key.php';
 
-$dbconn = pg_connect("host=localhost dbname=greouxlocation user=greouxlocation password=".BASE_PWD)
-    or die('Connexion impossible : ' . pg_last_error());
+$dbconn = pg_connect("host=localhost dbname=greouxlocation user=greouxlocation password=" . BASE_PWD)
+        or die('Connexion impossible : ' . pg_last_error());
 
-if($mode == cancel){
+if ($mode == cancel) {
 
-$id_book = $_GET['id'];
+    $id_book = $_GET['id'];
 
-$query = "DELETE FROM bookings WHERE id_booking = $id_book";
-echo $query;
-$result = pg_query($query) or die('Échec requête : ' . pg_last_error());
+    $query = "DELETE FROM bookings WHERE id_booking = $id_book";
+    echo $query;
+    $result = pg_query($query) or die('Échec requête : ' . pg_last_error());
 
-echo "OPERATION EFFECTUEE : reservation eliminee";
-}
+    echo "OPERATION EFFECTUEE : reservation eliminee";
+} else {
 
-else {
+    $client = $_POST['client'];
 
-$client = $_POST['client'];
+    $query = "INSERT INTO bookings (client, check_in, check_out) VALUES ('$client', '$check_in', '$check_out')";
+    echo $query;
+    $result = pg_query($query) or die('Échec requête : ' . pg_last_error());
 
-$query = "INSERT INTO bookings (client, check_in, check_out) VALUES ('$client', '$check_in', '$check_out')";
-echo $query;
-$result = pg_query($query) or die('Échec requête : ' . pg_last_error());
-
-echo "OPERATION EFFECTUEE : reservation enregistree";
+    echo "OPERATION EFFECTUEE : reservation enregistree";
 }
 
 $line = pg_fetch_assoc($result);
@@ -34,5 +32,4 @@ pg_close($dbconn);
 echo '<script language="Javascript">
 document.location.replace("manager.php");
 </script>';
-
 ?>
