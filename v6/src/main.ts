286f79a33404daeb8154a771d6e84d6402bef132
a76597 <gregory.anne@bnpparibas.fr>
2017-07-12 10:39:18 +0200

    fix internet explorer

--- v6/src/main.ts
+++ v6/src/main.ts
@@ -4,9 +4,22 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
+let getParameterByName = function(name, url) {
+    if (!url){
+         url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-let url = new URL("http://xxx" + window.location.search);
-let loc = url.searchParams.get("locale");
+let loc = getParameterByName("locale", null);
 if (loc === undefined || loc === "" || loc === null) {
     loc = navigator['language'] || navigator['userLanguage'];
 }
