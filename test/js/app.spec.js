7f2a2923e117f1237e297a82b6f5b8b467ee9a4f
Gregory Anne <englishblazere@free.fr>
2017-09-05 09:21:20 +0200

    now there is a package-lock file, maybe we can restore the package.json as it was

---
+++ test/js/app.spec.js
@@ -0,0 +1,10 @@
+describe('app.js spec', function () {
+
+    describe('when view is constructing', function () {
+        it('should exist', function () {
+            expect(view).toBeDefined();
+        });
+    });
+
+
+});
