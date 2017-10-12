fa61172b7b7b9eb9983b51521b77ad07be8e91e2
Gregory Anne <englishblazere@free.fr>
2017-10-12 19:56:20 +0200

    protractor timeout

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,10 @@
 // conf.js
 exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/js/app.spec.js']
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['test/js/app.spec.js'],
+    onPrepare: function () {
+        // Override the timeout for webdriver.
+        ptor.driver.manage().timeouts().setScriptTimeout(60000);
+    }
 }
