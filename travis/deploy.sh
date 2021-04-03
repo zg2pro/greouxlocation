b5edc923479685a4f49880ed6ab0da75a1f6f87f
Gregory Anne <englishblazere@free.fr>
2021-04-03 11:49:48 +0200

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,5 +2,5 @@
 cd dist
 mkdir assets/security
 echo "<?php define('BASE_PWD', '$GREOUXLOCATION_FTP_PWD'); ?>" > assets/security/key.php
-find * -type f -exec curl -u $FTP_USER:$GREOUXLOCATION_FTP_PWD --ftp-create-dirs -T {} ftp://ftpperso.free.fr/{} \;
+find * -type f -exec curl -u $FTP_USER:$GREOUXLOCATION_FTP_PWD --ftp-create-dirs -T {} ftp://ftpupload.net/{} \;
 cd ..
