b26719233108141ee39eca7dcf5d89416d43dcfd
Gregory Anne <englishblazere@free.fr>
2021-04-03 15:43:00 +0000

    try-gh-pages-deploy

--- travis/build.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-npm run clean && npm run build:prod
--- travis/deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-cd dist
-mkdir assets/security
-echo "<?php define('BASE_PWD', '$GREOUXLOCATION_FTP_PWD'); ?>" > assets/security/key.php
-find * -type f -exec curl -u $FTP_USER:$GREOUXLOCATION_FTP_PWD --ftp-create-dirs -T {} ftp://ftpupload.net/{} \;
-cd ..
